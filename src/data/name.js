57680b5b6349f58e8ecf623fbb779bf1567c09c2
Suhaib-Sirajudeen <suhaibgdr029@gmail.com>
2024-10-27 22:27:18 +0530

    created name array of string

---
+++ src/data/name.js
@@ -0,0 +1,12 @@
+export const names = [
+  "Aiden", "Bella", "Carter", "Daisy", "Ethan", "Fiona", "Grace", "Henry", "Isla", "Jack",
+  "Kara", "Liam", "Mia", "Noah", "Olivia", "Paul", "Quinn", "Ruby", "Sophia", "Tyler",
+  "Uma", "Violet", "Wyatt", "Xander", "Yara", "Zoe", "Aaron", "Brooke", "Charlie", "Dylan",
+  "Emma", "Finn", "Gabriel", "Hannah", "Isaac", "Jade", "Kevin", "Lucy", "Mason", "Nora",
+  "Oscar", "Piper", "Quincy", "Riley", "Samuel", "Tara", "Ursula", "Vincent", "Willow", "Xena",
+  "Yvonne", "Zachary", "Alice", "Ben", "Cleo", "David", "Eva", "Felix", "George", "Hailey",
+  "Ian", "Julia", "Kyle", "Lily", "Max", "Nina", "Owen", "Penelope", "Quentin", "Rose",
+  "Simon", "Tracy", "Ulrich", "Vera", "Wade", "Ximena", "Yosef", "Zoey", "Amber", "Blake",
+  "Cindy", "Dexter", "Elena", "Frank", "Gina", "Hugo", "Ivy", "Jake", "Kim", "Luna",
+  "Miles", "Nate", "Olive", "Peter", "Queen", "Ron", "Sophie", "Tom", "Ulysses", "Valerie"
+];
