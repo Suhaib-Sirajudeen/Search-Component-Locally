5cee1bda7eb2b11487b37dd746a63141f6794f1a
Suhaib-Sirajudeen <suhaibgdr029@gmail.com>
2024-10-27 23:41:17 +0530

    giving focus using useRef Hook

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,13 +1,23 @@
 import'./SearchInput.css'
 import closeIcon from '../../assets/closeIcon.svg'
+import { useEffect, useRef } from 'react'
 
 export const SearchInput=({userInput , handleInput , clearAll})=>{
 
-  
+  const inputRef =useRef(null);
 
+  useEffect(()=>{
+
+   inputRef.current.focus();
+  }
+  
+  
+  ,[userInput])
+  
+ 
   return(
     <div className="search-input-section">
-      <input placeholder='Enter the Name ...' value={userInput} onChange={handleInput}/>
+      <input placeholder='Enter the Name ...' value={userInput} onChange={handleInput} ref={inputRef}/>
       {userInput && 
 
        <button className='close-icon' onClick={clearAll}> 
