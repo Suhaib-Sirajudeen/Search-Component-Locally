8fd12b473da8cf7dc2faa2dac9a9e6d50220e991
Suhaib-Sirajudeen <suhaibgdr029@gmail.com>
2024-10-27 23:13:09 +0530

    added clear ibutton for search-input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,4 +1,5 @@
 import'./SearchInput.css'
+import closeIcon from '../../assets/closeIcon.svg'
 
 export const SearchInput=({userInput , handleInput})=>{
 
@@ -7,6 +8,16 @@ export const SearchInput=({userInput , handleInput})=>{
   return(
     <div className="search-input-section">
       <input placeholder='Enter the Name ...' value={userInput} onChange={handleInput}/>
+      {userInput && 
+
+       <button className='close-icon'> 
+        
+          <img src={closeIcon} alt="" width='32px' height='32px' />
+       
+       </button>
+      
+      }
+     
     </div>
   )
 }
